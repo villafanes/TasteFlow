eb8df35f2ffe6f0037913005bbb657451f2b2882
villafanes <villafanesani@gmail.com>
2024-07-15 17:50:06 -0400

    Condensed functions, updated main

---
+++ data_fcts.py
@@ -0,0 +1,35 @@
+#https://www.geeksforgeeks.org/how-to-merge-multiple-json-files-using-python/
+#import json
+#import os
+
+# condenses the multiple files downloaded from Spotify
+def merge_files(file_paths, output_file):
+    if os.path.exists(output_file):
+        os.remove(output_file)
+
+    merged_data = []
+    for path in file_paths:
+        with open(path, 'r') as in_file:
+            data = json.load(in_file)
+            merged_data.extend(data)
+    with open(output_file, 'w') as outfile:
+        json.dump(merged_data, outfile, indent=2)
+
+
+# processes data in the new, condensed file to isolate only Date, Song, Artist, and Album
+def process_file(in_file):
+    with open(in_file, 'r') as file:
+        data = json.load(file)
+    
+    song_dict = []
+    for entry in data:
+        for instance in entry:
+            formatted_instance = {
+                "Date": entry["ts"][:10],  # Extract YYYY-MM-DD from ts
+                "Song": entry["master_metadata_track_name"],
+                "Artist": entry["master_metadata_album_artist_name"],
+                "Album": entry["master_metadata_album_album_name"]
+            }
+            song_dict.append(formatted_instance)
+
+    return song_dict
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from merge_files_fct import merge_files
+import json
+import os
+from data_fcts import merge_files, process_file
 
-
+# Update to take in user file paths
 file_paths = ["SH_20_21.json", "SH_21_22.json", "SH_22_23.json", "SH_23_24.json"]
 file = "full_spotify_streaming_history_samvilla.json"
 merge_files(file_paths, file)
 print(f"Merged data written to '{file}")
 
+proc_file = process_file(file)
