0c50025906207d1bdd60244de12eb7493a77b096
villafanes <villafanesani@gmail.com>
2024-07-15 22:55:45 -0400

    streams_by_artist function definition

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from data_fcts import merge_files, process_file
 
 # Update to take in user file paths
 file_paths = ["SH_20_21.json", "SH_21_22.json", "SH_22_23.json", "SH_23_24.json"]
-file = "full_spotify_streaming_history_samvilla.json"
+file = "full_spotify_streaming_history.json"
 merge_files(file_paths, file)
 print(f"Merged data written to '{file}")
 
