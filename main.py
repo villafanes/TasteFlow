7317d046ef4e9ef43aae932feb67778b64a09a01
villafanes <villafanesani@gmail.com>
2024-07-19 19:09:44 -0400

    creating streamlit app

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 import json
 import os
 from data_fcts import *
+import pandas as pd
+import streamlit as st
 
-# Update to take in user file paths
-file_paths = ["SH_20_21.json", "SH_21_22.json", "SH_22_23.json", "SH_23_24.json"]
-file = "full_spotify_streaming_history.json"
-merge_files(file_paths, file)
-print(f"Merged data written to '{file}")
+st.title('Full Spotify Listening History')
 
-# processed list of dictionaries of each stream
-streams_dict = process_file(file)
+files = st.file_uploader("Upload JSON listening files", type="json", accept_multiple_files=True)
 
-# dict of streaming instances by artist
-streams_per_artist = streams_by_artist(streams_dict)
-
-# dict of artists and all-time stream counts
-artist_total_streams = artist_ct(streams_per_artist)
-
-# dict of artists and stream counts by year
-artist_yearly_streams = artist_yr_ct(streams_per_artist)
-
-# dict of streaming instances by album
-streams_per_album = streams_by_album(streams_dict)
-
-# dict of streaming instances by album
-streams_per_song = streams_by_song(streams_dict)            
