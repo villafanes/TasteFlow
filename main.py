e44060f1c4363a800bb21fc8b3140d82466068a5
villafanes <villafanesani@gmail.com>
2024-07-19 19:12:03 -0400

    converting files to pandas data frames

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ st.title('Full Spotify Listening History')
 
 files = st.file_uploader("Upload JSON listening files", type="json", accept_multiple_files=True)
 
+if files:
+    # storing pandas data frames
+    data_frames = []
+
+    for file in files:
+        content = file.read()
+        data = json.loads(content)
+
+        # converting JSON to DataFrame 
+        df = pd.json_normalize(data)
+        data_frames.append(df)
+
+    # combining all DataFrames into one
+    streams_df = pd.concat(data_frames, ignore_index=True)
+    
+    # display
+    st.write(streams_df)
